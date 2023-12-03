8814b4f8af678e5463b998a7dd1382854fff7bd4
RR7B7 <fedotovkirill4000@gmail.com>
2023-12-03 22:51:45 +0300

    analyzing how bmi affects level of cholesterol

--- calc_tasks/parenhima_thickness_decrease.py
+++ calc_tasks/parenhima_thickness_decrease.py
@@ -4,6 +4,7 @@ from preformatting_data import *
 
 data = preformat(pd.read_csv('../medics_1.csv', delimiter=',', encoding='utf-8'))
 
+# берем данные, где нет хбп, строим график скаттер для выявления зависимости
 data_without_hbp = data.loc[data['хбп'] == 0]
 viz_data_scatter(data_without_hbp, 'возраст',
                  'толщина_паренхимы_почек')
--- calc_tasks/surgery_duration_and_heart_attack.py
+++ calc_tasks/surgery_duration_and_heart_attack.py
@@ -4,4 +4,5 @@ from preformatting_data import *
 
 data = preformat(pd.read_csv('../medics_1.csv', delimiter=',', encoding='utf-8'))
 
+# строим ящик с усами для выявления зависимости между инфарктом ранее и длительностю операции
 viz_data_box(data, 'инфаркт_миокарда', 'длительность_операции')
