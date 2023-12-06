1a9eaec91e97c8e634d35897f0d5ac307ba8521c
RR7B7 <fedotovkirill4000@gmail.com>
2023-12-06 21:49:35 +0300

    doing tasks

--- calc_tasks/surgery_duration_and_heart_attack.py
+++ calc_tasks/surgery_duration_and_heart_attack.py
@@ -1,8 +1,14 @@
 import pandas as pd
 from graphics import *
 from preformatting_data import *
+from scipy.stats import shapiro, pearsonr, chi2_contingency
+import numpy as np
 
 data = preformat(pd.read_csv('../medics_1.csv', delimiter=',', encoding='utf-8'))
 
 # строим ящик с усами для выявления зависимости между инфарктом ранее и длительностю операции
 viz_data_box(data, 'инфаркт_миокарда', 'длительность_операции')
+
+cross_table = pd.crosstab(data['инфаркт_миокарда'], data['длительность_операции'])
+print(round(cramers_v(cross_table), 4))
+
--- tools.py
+++ tools.py
@@ -1,4 +1,10 @@
+from scipy.stats import chi2_contingency
+import numpy as np
+
+
 def convert_to_float(x):
     return float(str(x).replace(',', '.'))
 
-print("test")
+def cramers_v(crosstab):
+    chi2 = chi2_contingency(crosstab)[0]
+    return np.sqrt(chi2 / (crosstab.sum().sum() * (min(crosstab.shape) - 1)))
