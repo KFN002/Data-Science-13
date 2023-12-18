700145dd7a6ba4646145ae959ff00afc0545cca4
RR7B7 <fedotovkirill4000@gmail.com>
2023-12-18 22:23:35 +0300

    working on hypothesises

---
+++ hypotheses/hypothesis_bmi.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from scipy.stats import ttest_ind, shapiro, mannwhitneyu
+from preformatting_data import preformat
+
+'''
+H0: Между группами с опп и без него нет разницы в имт
+'''
+
+data = preformat(pd.read_csv('../medics_1.csv', delimiter=',', encoding='utf-8'))
+
+# разделение групп на опп и без него
+with_opp = data[data['развитие_опп'] == 1]
+without_opp = data[data['развитие_опп'] == 0]
+
+# подсчет метрик в зависимости от распределения
+if shapiro(with_opp["имт"])[1] >= 0.05 and shapiro(without_opp["имт"])[1] >= 0.05:
+    t_stat, p_value = ttest_ind(with_opp['имт'], without_opp['имт'], equal_var=False)
+else:
+    t_stat, p_value = mannwhitneyu(with_opp['имт'], without_opp['имт'])
+
+# выводим метрики
+print(f'T-statistic: {round(t_stat, 4)}')
+print(f'P-value: {round(p_value, 4)}')
+
+print('------------------------')
+
+if p_value < 0.05:
+    print("Отвергаем нулевую гипотезу, различия между группами существуют.")
+else:
+    print("Не получилось отвергнуть нулевую гипотезу, различия между группами не существенны.")
---
+++ hypotheses/hypothesis_cat_blood_aik.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from scipy.stats import ttest_ind, shapiro, mannwhitneyu, chi2_contingency
+from preformatting_data import preformat
+
+
+'''
+H0: Между группами с опп и без него нет разницы в наличии хронических заболеваний
+'''
+
+data = preformat(pd.read_csv('../medics_1.csv', delimiter=',', encoding='utf-8'))
+
+data["развитие_опп"] = data["развитие_опп"].apply(lambda x: 'да' if x == 1 else 'нет')
+
+
+# подсчет метрик
+contingency_table = pd.crosstab(data["развитие_опп"], data['переливание_крови_и_аик'])
+chi2, p_value, _, _ = chi2_contingency(contingency_table)
+
+# выводим метрики
+print(f'Chi2: {round(chi2, 4)}')
+print(f'P-value: {round(p_value, 4)}')
+
+print('------------------------')
+
+if p_value < 0.05:
+    print("Отвергаем нулевую гипотезу, различия между группами существуют.")
+else:
+    print("Не получилось отвергнуть нулевую гипотезу, различия между группами не существенны.")
